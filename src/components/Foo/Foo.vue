64a17c6cd3e30a307d19285c8ad364bdd516127f
Fatih Çınar <fthcnr1907@gmail.com>
2020-04-18 00:46:36 +0300

    Initial commit

--- src/components/Foo/Foo.vue
+++ src/components/Foo/Foo.vue
@@ -1,22 +1,31 @@
 <template>
     <div id="foo">
         <Navbar />
-    
+        <div v-for="(user, index) in users" :key="index">
+            <UserItem :user=user />
+        </div>
     </div>
 </template>
 
 <script>
 import Navbar from './Navbar'
+import UserItem from './UserItem'
 
 export default {
     name: 'Foo',
     components: {
-        Navbar
+        Navbar,
+        UserItem
+    },
+    data() {
+        return {
+            users: []
+        }
     },
     created() {
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(json => console.log(json))
+            .then(users => this.users = users)
     }
 }
 </script>
---
+++ src/components/Foo/UserItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>{{ user.name }} @{{ user.username }}</p>
+        <p></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserItem',
+    props: ['user'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
